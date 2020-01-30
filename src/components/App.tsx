72af5f4088373a4362d5deafb4e4335913f1f074
ramp4 <rampatv13@gmail.com>
2020-01-30 23:44:49 +0300

    feat: implement adaptive logo, change ico+logo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import TicketsContainer from "../containers/TicketsContainer";
 import FilterContainer from "../containers/FilterContainer";
 import "./App.scss";
-import logoSrc from "../assets/logo.svg";
+import logoSrc from "../data/logo.svg";
 
 class App extends React.Component {
   render() {
