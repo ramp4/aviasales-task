077bb720c62c747e345ca0a08645a11f7fc938db
ramp4 <rampatv13@gmail.com>
2020-01-30 18:52:09 +0300

    checkbox

--- src/components/FilterOption.tsx
+++ src/components/FilterOption.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./FilterOption.scss";
+
 interface FilterOptionProps {
   id: string;
   name: string;
@@ -10,18 +11,21 @@ interface FilterOptionProps {
 
 const FilterOption: React.FC<FilterOptionProps> = props => {
   return (
-    <label className={`${props.name} filter_option`}>
-      <input
-        id={props.id}
-        className={`${props.name}__option filter_checkbox`}
-        type="checkbox"
-        name={`${props.name}__option`}
-        value={props.name}
-        onChange={props.onChange}
-        checked={props.checked}
-      />
-      {props.text}
-    </label>
+    <div className={`${props.name} filter_option control-group`}>
+      <label htmlFor={props.id} className="control-checkbox control">
+        <div className="control_indicator"></div>
+        <input
+          id={props.id}
+          className={`${props.name}__option filter_checkbox checkbox`}
+          type="checkbox"
+          name={`${props.name}__option`}
+          value={props.name}
+          onChange={props.onChange}
+          checked={props.checked}
+        />
+        {props.text}
+      </label>
+    </div>
   );
 };
 
