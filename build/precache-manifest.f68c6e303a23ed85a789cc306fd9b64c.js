529e723ecc5532ac9f4bb257c71764b2e6a1f1a0
ramp4 <rampatv13@gmail.com>
2020-01-30 21:36:25 +0300

    add build

---
+++ build/precache-manifest.f68c6e303a23ed85a789cc306fd9b64c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5ff734ca15ae4d60c230a973e35056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a36f8084d4b9e80f060",
+    "url": "/static/css/main.003c4159.chunk.css"
+  },
+  {
+    "revision": "8844145a040f9b0e6dd5",
+    "url": "/static/js/2.0581e830.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.0581e830.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a36f8084d4b9e80f060",
+    "url": "/static/js/main.8b4c201d.chunk.js"
+  },
+  {
+    "revision": "3632935b1cde1626db33",
+    "url": "/static/js/runtime-main.d9f74c69.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/static/media/logo.0c089234.svg"
+  }
+]);
